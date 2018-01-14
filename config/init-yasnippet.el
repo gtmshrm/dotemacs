8d9801bf9e0b0fac9dbf2005de6dc20d12e91cf1
Gautam Sharma <gautamsharma7234@gmail.com>
2018-01-14 22:53:23 +0530

    enabled yasnippet globally

--- config/init-yasnippet.el
+++ config/init-yasnippet.el
@@ -7,9 +7,13 @@
  (setq yas-also-auto-indent-first-line t)
  (setq yas-prompt-functions '(yas/ido-prompt yas/completing-prompt))
 
- (add-hook 'prog-mode-hook 'yas-minor-mode)
- (add-hook 'org-mode-hook 'yas-minor-mode)
- (add-hook 'html-mode-hook 'yas-minor-mode)
+ ;; Enable for specific modes
+ ;; (add-hook 'prog-mode-hook 'yas-minor-mode)
+ ;; (add-hook 'org-mode-hook 'yas-minor-mode)
+ ;; (add-hook 'html-mode-hook 'yas-minor-mode)
+
+ ;; Enable globally
+ (yas-global-mode 1)
 
  (yas-load-directory (concat user-emacs-directory "snippets")))
 
