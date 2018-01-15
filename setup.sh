0338b74f98b06aaba640b45ab93154f75924222a
Gautam Sharma <gautamsharma7234@gmail.com>
2018-01-15 19:39:52 +0530

    added gnu stow for setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/sh
 
 files=(bin config core elisp init.el snippets .eshell-aliases)
 
@@ -6,6 +6,6 @@ mkdir -p ~/.emacs.d
 
 echo "Creating symlinks..."
 for file in $files; do
-	ln -s $PWD/$file ~/.emacs.d/$file
+	stow $file -t ~/.emacs.d
 done
 echo "Done."
