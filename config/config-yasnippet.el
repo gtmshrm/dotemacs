3cab8b4fba417620d1055d463ab57cae993b364e
Gautam Sharma <gautamsharma7234@gmail.com>
2018-01-22 15:23:50 +0530

    added yasnippet's official snippet collection

--- config/config-yasnippet.el
+++ config/config-yasnippet.el
@@ -3,6 +3,8 @@
 
  (require 'yasnippet)
 
+ (require-package 'yasnippet-snippets)
+
  (setq yas-fallback-behavior 'return-nil)
  (setq yas-also-auto-indent-first-line t)
  (setq yas-prompt-functions '(yas/ido-prompt yas/completing-prompt))
